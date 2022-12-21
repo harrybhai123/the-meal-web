23d6722981904329edb33beaa57f40f858ebd483
Hariom Tomar <68967590+harrybhai123@users.noreply.github.com>
2022-12-21 12:44:24 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,145 @@
+let items = [
+  {
+    id: 1,
+    item: "Croatian Bean Stew",
+    category: "Beef",
+    area: "Croatian",
+    instructions:
+      "Heat the oil in a pan. Add the chopped vegetables and sauté until tender. Take a pot, empty the beans together with the vegetables into it, put the sausages inside and cook for further 20 minutes on a low heat. Or, put it in an oven and bake it at a temperature of 180ºC/350ºF for 30 minutes. This dish is even better reheated the next day.",
+    MealThumb:
+      "https://www.themealdb.com/images/media/meals/tnwy8m1628770384.jpg",
+  },
+  {
+    id: 2,
+    item: "General Tso's Chicken",
+    category: "Chicken",
+    area: "Chinese",
+    instructions:
+      "DIRECTIONS:\r\nSTEP 1 - SAUCE\r\nIn a bowl, add 2 Cups of water, 2 Tablespoon soy sauce, 2 Tablespoon white vinegar, sherry cooking wine, 1/4 Teaspoon white pepper, minced ginger, minced garlic, hot pepper, ketchup, hoisin sauce, and sugar.\r\nMix together well and set aside.\r\nSTEP 2 - MARINATING THE CHICKEN\r\nIn a bowl, add the chicken, 1 pinch of salt, 1 pinch of white pepper, 2 egg whites, and 3 Tablespoon of corn starch\r\nSTEP 3 - DEEP FRY THE CHICKEN\r\nDeep fry the chicken at 350 degrees for 3-4 minutes or until it is golden brown and loosen up the chicken so that they don't stick together.\r\nSet the chicken aside.\r\nSTEP 4 - STIR FRY\r\nAdd the sauce to the wok and then the broccoli and wait until it is boiling.\r\nTo thicken the sauce, whisk together 2 Tablespoon of cornstarch and 4 Tablespoon of water in a bowl and slowly add to your stir-fry until it's the right thickness.\r\nNext add in the chicken and stir-fry for a minute and serve on a plate",
+    MealThumb: "https://www.themealdb.com/images/media/meals/1529444113.jpg",
+  },
+  {
+    id: 3,
+    item: "Nasi lemak",
+    category: "Seafood",
+    area: "Malaysian",
+    instructions:
+      "In a medium saucepan over medium heat, stir together coconut milk, water, ground ginger, ginger root, salt, bay leaf, and rice. Cover, and bring to a boil. Reduce heat, and simmer for 20 to 30 minutes, or until done.\r\n\r\n Step 2\r\nPlace eggs in a saucepan, and cover with cold water. Bring water to a boil, and immediately remove from heat. Cover, and let eggs stand in hot water for 10 to 12 minutes. Remove eggs from hot water, cool, peel and slice in half. Slice cucumber.\r\n\r\n Step 3\r\nMeanwhile, in a large skillet or wok, heat 1 cup vegetable oil over medium-high heat. Stir in peanuts and cook briefly, until lightly browned. Remove peanuts with a slotted spoon and place on paper towels to soak up excess grease. Return skillet to stove. Stir in the contents of one package anchovies; cook briefly, turning, until crisp. Remove with a slotted spoon and place on paper towels. Discard oil. Wipe out skillet.\r\n\r\n Step 4\r\nHeat 2 tablespoons oil in the skillet. Stir in the onion, garlic, and shallots; cook until fragrant, about 1 or 2 minutes. Mix in the chile paste, and cook for 10 minutes, stirring occasionally. If the chile paste is too dry, add a small amount of water. Stir in remaining anchovies; cook for 5 minutes. Stir in salt, sugar, and tamarind juice; simmer until sauce is thick, about 5 minutes.\r\n\r\n Step 5\r\nServe the onion and garlic sauce over the warm rice, and top with peanuts, fried anchovies, cucumbers, and eggs.",
+    MealThumb:
+      "https://www.themealdb.com/images/media/meals/wai9bw1619788844.jpg",
+  },
+  {
+    id: 4,
+    item: "Chicken Congee",
+    category: "Chicken",
+    area: "Chinese",
+    instructions:
+      "STEP 1 - MARINATING THE CHICKEN\r\nIn a bowl, add chicken, salt, white pepper, ginger juice and then mix it together well.\r\nSet the chicken aside.\r\nSTEP 2 - RINSE THE WHITE RICE\r\nRinse the rice in a metal bowl or pot a couple times and then drain the water.\r\nSTEP 2 - BOILING THE WHITE RICE\r\nNext add 8 cups of water and then set the stove on high heat until it is boiling. Once rice porridge starts to boil, set the stove on low heat and then stir it once every 8-10 minutes for around 20-25 minutes.\r\nAfter 25 minutes, this is optional but you can add a little bit more water to make rice porridge to make it less thick or to your preference.\r\nNext add the marinated chicken to the rice porridge and leave the stove on low heat for another 10 minutes.\r\nAfter an additional 10 minutes add the green onions, sliced ginger, 1 pinch of salt, 1 pinch of white pepper and stir for 10 seconds.\r\nServe the rice porridge in a bowl\r\nOptional: add Coriander on top of the rice porridge.",
+    MealThumb: "https://www.themealdb.com/images/media/meals/1529446352.jpg",
+  },
+  {
+    id: 5,
+    item: "Chicken Fajita Mac and Cheese",
+    category: "Chicken",
+    area: "American",
+    instructions:
+      "Fry your onion, peppers and garlic in olive oil until nicely translucent. Make a well in your veg and add your chicken. Add your seasoning and salt. Allow to colour slightly.\r\nAdd your cream, stock and macaroni.\r\nCook on low for 20 minutes. Add your cheeses, stir to combine.\r\nTop with roasted peppers and parsley.",
+    MealThumb:
+      "https://www.themealdb.com/images/media/meals/qrqywr1503066605.jpg",
+  },
+  {
+    id: 6,
+    item: "Ratatouille",
+    category: "Vegetarian",
+    area: "French",
+    instructions:
+      "Cut the aubergines in half lengthways. Place them on the board, cut side down, slice in half lengthways again and then across into 1.5cm chunks. Cut off the courgettes ends, then across into 1.5cm slices. Peel the peppers from stalk to bottom. Hold upright, cut around the stalk, then cut into 3 pieces. Cut away any membrane, then chop into bite-size chunks.\r\nScore a small cross on the base of each tomato, then put them into a heatproof bowl. Pour boiling water over the tomatoes, leave for 20 secs, then remove. Pour the water away, replace the tomatoes and cover with cold water. Leave to cool, then peel the skin away. Quarter the tomatoes, scrape away the seeds with a spoon, then roughly chop the flesh.\r\nSet a sauté pan over medium heat and when hot, pour in 2 tbsp olive oil. Brown the aubergines for 5 mins on each side until the pieces are soft. Set them aside and fry the courgettes in another tbsp oil for 5 mins, until golden on both sides. Repeat with the peppers. Don’t overcook the vegetables at this stage, as they have some more cooking left in the next step.\r\nTear up the basil leaves and set aside. Cook the onion in the pan for 5 mins. Add the garlic and fry for a further min. Stir in the vinegar and sugar, then tip in the tomatoes and half the basil. Return the vegetables to the pan with some salt and pepper and cook for 5 mins. Serve with basil.",
+    MealThumb:
+      "https://www.themealdb.com/images/media/meals/wrpwuu1511786491.jpg",
+  },
+  {
+    id: 7,
+    item: "Eton Mess",
+    category: "Dessert",
+    area: "British",
+    instructions:
+      "Purée half the strawberries in a blender. Chop the remaining strawberries, reserving four for decoration.\r\nWhip the double cream until stiff peaks form, then fold in the strawberry purée and crushed meringue. Fold in the chopped strawberries and ginger cordial, if using.\r\nSpoon equal amounts of the mixture into four cold wine glasses. Serve garnished with the remaining strawberries and a sprig of mint.",
+    MealThumb:
+      "https://www.themealdb.com/images/media/meals/uuxwvq1483907861.jpg",
+  },
+  {
+    id: 8,
+    item: "Spotted Dick",
+    category: "Dessert",
+    area: "British",
+    instructions:
+      "Put the flour and salt in a bowl. Add the suet, currants, sugar, lemon and orange zest.\r\nPour in 150ml milk and mix to a firm but moist dough, adding the extra milk if necessary.\r\nShape into a fat roll about 20cm long. Place on a large rectangle of baking parchment. Wrap loosely to allow for the pudding to rise and tie the ends with string like a Christmas cracker.\r\nPlace a steamer over a large pan of boiling water, add the pudding to the steamer, cover and steam for 1 1/2 hours. Top up the pan with water from time to time.\r\nRemove from the steamer and allow to cool slightly before unwrapping. Serve sliced with custard.",
+    MealThumb:
+      "https://www.themealdb.com/images/media/meals/xqvyqr1511638875.jpg",
+  },
+  {
+    id: 9,
+    item: "Breakfast Potatoes",
+    category: "Breakfast",
+    area: "Canadian",
+    instructions:
+      "Before you do anything, freeze your bacon slices that way when you're ready to prep, it'll be so much easier to chop!\r\nWash the potatoes and cut medium dice into square pieces. To prevent any browning, place the already cut potatoes in a bowl filled with water.\r\nIn the meantime, heat 1-2 tablespoons of oil in a large skillet over medium-high heat. Tilt the skillet so the oil spreads evenly.\r\nOnce the oil is hot, drain the potatoes and add to the skillet. Season with salt, pepper, and Old Bay as needed.\r\nCook for 10 minutes, stirring the potatoes often, until brown. If needed, add a tablespoon more of oil.\r\nChop up the bacon and add to the potatoes. The bacon will start to render and the fat will begin to further cook the potatoes. Toss it up a bit! The bacon will take 5-6 minutes to crisp.\r\nOnce the bacon is cooked, reduce the heat to medium-low, add the minced garlic and toss. Season once more. Add dried or fresh parsley. Control heat as needed.\r\nLet the garlic cook until fragrant, about one minute.\r\nJust before serving, drizzle over the maple syrup and toss. Let that cook another minute, giving the potatoes a caramelized effect.\r\nServe in a warm bowl with a sunny side up egg!",
+    MealThumb: "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+  },
+  {
+    id: 10,
+    item: "Tandoori chicken",
+    category: "Chicken",
+    area: "Indian",
+    instructions:
+      "Mix the lemon juice with the paprika and red onions in a large shallow dish. Slash each chicken thigh three times, then turn them in the juice and set aside for 10 mins.\r\nMix all of the marinade ingredients together and pour over the chicken. Give everything a good mix, then cover and chill for at least 1 hr. This can be done up to a day in advance.\r\nHeat the grill. Lift the chicken pieces onto a rack over a baking tray. Brush over a little oil and grill for 8 mins on each side or until lightly charred and completely cooked through.",
+    MealThumb:
+      "https://www.themealdb.com/images/media/meals/qptpvt1487339892.jpg",
+  },
+];
+
+const item = document.getElementById("item");
+const category = document.getElementById("category");
+const area = document.getElementById("area");
+const instructions = document.getElementById("instructions");
+const img = document.getElementById("img");
+const prev = document.getElementById("prev");
+const random = document.getElementById("random");
+const next = document.getElementById("next");
+
+let current = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+  allMeal(current);
+});
+
+const allMeal = () => {
+  let mealItems = items[current];
+  item.textContent = "Meal Name:- " + mealItems.item;
+  category.textContent = "Meal Category:- " + mealItems.category;
+  area.textContent = "Meal Area:- " + mealItems.area;
+  instructions.innerHTML =
+    "<strong> Meal instructions:- </strong>" + mealItems.instructions;
+  img.src = mealItems.MealThumb;
+  console.log(mealItems.item);
+};
+
+random.addEventListener("click", function () {
+  current = Math.trunc(Math.random() * items.length);
+  allMeal(current);
+});
+
+prev.addEventListener("click", function () {
+  current--;
+  if (current < 0) {
+    current = items.length - 1;
+  }
+  allMeal(current);
+});
+next.addEventListener("click", function () {
+  current++;
+  if (current > items.length - 1) {
+    current = 0;
+  }
+  allMeal(current);
+});
